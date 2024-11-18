3ae66b009c18d5c5e6606797f4426c2f734ef015
Michael Vinogradov <michaelvin1322@gmail.com>
2024-11-18 15:50:40 +0100

    [Fix]: Solved the problem with grouped columns

--- src/data/convert_raw_to_int.py
+++ src/data/convert_raw_to_int.py
@@ -40,7 +40,6 @@ def load_and_clean_csv(filename: str, index_col: Optional[str] = None) -> pd.Dat
         pd.DataFrame: A cleaned Pandas DataFrame with duplicates removed and the index set.
     """
     df = pd.read_csv(filename, index_col=index_col)
-
     # Drop rows with NaN in the index
     df = df[~df.index.isnull()]
     # Remove duplicate index entries, keeping the first occurrence
@@ -70,6 +69,9 @@ def load_and_merge_datasets(cfg: DictConfig) -> pd.DataFrame:
     # Merging datasets step-by-step
     merged_df = merge_jobs_with_work_history(jobs_df, work_history_df)
     merged_df = merge_with_companies(merged_df, companies_df)
+    merged_df = merge_segmentation_data(merged_df, segmentation_data_df)
+    merged_df = merge_with_occupations(merged_df, occupations_df)
+
     merged_df = merge_with_grouped_data(
         merged_df=merged_df,
         df=additional_skills_df,
@@ -86,19 +88,11 @@ def load_and_merge_datasets(cfg: DictConfig) -> pd.DataFrame:
     )
     merged_df = merge_with_grouped_data(
         merged_df=merged_df,
-        df=occupations_df,
-        key='JOBID',
-        value_col='PREFLABEL',
-        new_col_name='OCCUPATIONS',
-    )
-    merged_df = merge_with_grouped_data(
-        merged_df=merged_df,
         df=job_skills_with_labels,
         key='JOBID',
         value_col='PREFLABEL',
         new_col_name='SKILLS',
     )
-    merged_df = merge_segmentation_data(merged_df, segmentation_data_df)
     return merged_df
 
 
@@ -137,6 +131,28 @@ def merge_with_companies(
     return merged_df
 
 
+def merge_with_occupations(
+    merged_df: pd.DataFrame,
+    occupations_df: pd.DataFrame,
+) -> pd.DataFrame:
+    """Merges with OCCUPATIONS table."""
+    index = merged_df.index
+    occupations_df = occupations_df[['JOBID', 'PREFLABEL']].rename(
+        columns={'PREFLABEL': 'OCCUPATION'},
+    )
+    merged_df = pd.merge(
+        merged_df,
+        occupations_df,
+        left_index=True,
+        right_on='JOBID',
+        how='left',
+    ).drop(
+        columns=['JOBID'],
+    )
+    merged_df.set_index(index, inplace=True)
+    return merged_df
+
+
 def merge_with_grouped_data(
     merged_df: pd.DataFrame,
     df: pd.DataFrame,
@@ -151,9 +167,12 @@ def merge_with_grouped_data(
         .reset_index()
         .rename(columns={value_col: new_col_name})
     )
+    index = merged_df.index
     merged_df = pd.merge(merged_df, grouped, left_index=True, right_on=key, how='left').drop(
         columns=[key],
     )
+    merged_df.set_index(index, inplace=True)
+    merged_df.set_index(merged_df.index, inplace=True)
     log.info(f'Length after adding {new_col_name}: {len(merged_df)}')
     return merged_df
 
@@ -162,47 +181,22 @@ def merge_segmentation_data(
     merged_df: pd.DataFrame,
     segmentation_data_df: pd.DataFrame,
 ) -> pd.DataFrame:
-    """Merges SEGMENTATIONDATA with grouped lists of tuples."""
-    grouped = (
-        segmentation_data_df.groupby('JOBID')[['NAME', 'VALUE', 'LABEL', 'TYPE', 'SORTORDER']]
-        .apply(
-            lambda x: list(x.itertuples(index=False, name=None)),
-        )
-        .reset_index()
-        .rename(columns={0: 'SEGMENTATIONDATA'})
+    """Merges with SEGMENTATIONDATA table."""
+    segmentation_data_df = segmentation_data_df[
+        ['JOBID', 'NAME', 'VALUE', 'LABEL', 'TYPE', 'SORTORDER']
+    ]
+    index = merged_df.index
+    merged_df = pd.merge(
+        merged_df,
+        segmentation_data_df.add_prefix('SEGMENTATION_DATA_'),
+        left_index=True,
+        right_on='SEGMENTATION_DATA_JOBID',
+        how='left',
+    ).drop(
+        columns=['SEGMENTATION_DATA_JOBID'],
     )
-    merged_df = pd.merge(merged_df, grouped, left_index=True, right_on='JOBID', how='left').drop(
-        columns=['JOBID'],
-    )
+    merged_df.set_index(index, inplace=True)
     log.info(f'Length after adding SEGMENTATIONDATA: {len(merged_df)}')
-    return merged_df
-
-
-def merge_ontology_skills(
-    merged_df: pd.DataFrame,
-    job_ontology_skills_df: pd.DataFrame,
-    ontology_skills_df: pd.DataFrame,
-) -> pd.DataFrame:
-    """Merges job skills with ontology skills by SKILLID."""
-    job_skills_with_labels = pd.merge(
-        job_ontology_skills_df,
-        ontology_skills_df[['ID', 'PREFLABEL']],
-        left_on='SKILLID',
-        right_on='ID',
-        how='inner',
-    )
-    grouped = (
-        job_skills_with_labels.groupby('JOBID')['PREFLABEL']
-        .apply(list)
-        .reset_index()
-        .rename(
-            columns={'PREFLABEL': 'ONTOLOGY_SKILLS'},
-        )
-    )
-    merged_df = pd.merge(merged_df, grouped, left_on='ID', right_on='JOBID', how='left').drop(
-        columns=['JOBID'],
-    )
-    log.info(f'Length after adding ONTOLOGY_SKILLS: {len(merged_df)}')
     return merged_df
 
 
@@ -255,7 +249,7 @@ def main(cfg: DictConfig) -> None:
 
     # Save the final processed dataset
     save_path = os.path.join(save_dir, 'jobs.csv')
-    df.to_csv(save_path, index=False)
+    df.to_csv(save_path, index=True)
 
     log.info('Complete')
 
