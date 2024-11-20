dba8b2309586f4d9e8c29957ca1c9b1d5f07e048
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-11-20 11:58:04 +0100

    [Feature]: implement country processor

--- src/data/location_processors.py
+++ src/data/location_processors.py
@@ -28,7 +28,7 @@ class CityProcessor:
         similarities = [(city, ratio(normalized_name, city)) for city in self.database]
         filtered_results = [item for item in similarities if item[1] >= threshold]
         final_result = sorted(filtered_results, key=lambda x: x[1], reverse=True)[:1]
-        final_result = [(city.capitalize(), score) for city, score in final_result]
+        final_result = [(city.title(), score) for city, score in final_result]
         return final_result
 
 
@@ -52,17 +52,18 @@ class CountryProcessor:
     def get_similar(self, name: str, threshold: float = 0.75) -> List[Tuple[str, float]]:
         normalized_name = self._normalize(name)
         similarities = []
-        for country, iso2, iso3 in self.database:
+        for country, iso3 in self.database:
             scores = [
                 (country, ratio(normalized_name, country)),
-                (country, ratio(normalized_name, iso2)),
                 (country, ratio(normalized_name, iso3)),
             ]
             best_match = max(scores, key=lambda x: x[1])
             similarities.append(best_match)
 
         filtered_results = [item for item in similarities if item[1] >= threshold]
-        return sorted(filtered_results, key=lambda x: x[1], reverse=True)[:1]
+        final_result = sorted(filtered_results, key=lambda x: x[1], reverse=True)[:1]
+        final_result = [(country.title(), score) for country, score in final_result]
+        return final_result
 
 
 if __name__ == '__main__':
@@ -80,13 +81,11 @@ if __name__ == '__main__':
             print(f'{city_name} -> No match found')
 
     country_processor = CountryProcessor(database_path=csv_path)
-    test_country_names = ['RUS', 'USA', 'Dnemark', 'Danmark', 'Denmarc', 'DEN']
+    test_country_names = ['RUS', 'USA', 'Uni states', 'Dnemark', 'Danmark', 'Denmarc', 'DNK']
     for country_name in test_country_names:
         result = country_processor.get_similar(country_name, threshold=0.75)
         if result:
             target_country_name, target_score = result[0]
-            print(
-                f'{country_name} -> {target_country_name.capitalize()} (Score: {target_score:.2f})',
-            )
+            print(f'{country_name} -> {target_country_name} (Score: {target_score:.2f})')
         else:
             print(f'{country_name} -> No match found')
