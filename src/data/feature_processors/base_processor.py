4af3c5c8bc91b9b1ee49b69b700d3603c5a8b247
Michael Vinogradov <michaelvin1322@gmail.com>
2024-12-09 22:43:56 +0100

    [Feature]: added logic to check if processor is fit

--- src/data/feature_processors/base_processor.py
+++ src/data/feature_processors/base_processor.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import pandas as pd
+from sklearn.exceptions import NotFittedError
 
 
 class BaseProcessor(ABC):
@@ -17,9 +18,28 @@ class BaseProcessor(ABC):
             column_name (str): The name of the column to process.
         """
         self.column_name = column_name
+        self._is_fit = False
+
+    @property
+    def is_fit(self) -> bool:
+        """Checks if the processor has been fit.
+
+        Returns:
+            bool: True if the processor is fit, False otherwise.
+        """
+        return self._is_fit
+
+    def fit(self, df: pd.DataFrame) -> None:
+        """Fits the processor to the data and marks it as fit.
+
+        Args:
+            df (pd.DataFrame): The input DataFrame to fit on.
+        """
+        self._fit(df)
+        self._is_fit = True
 
     @abstractmethod
-    def fit(self, df: pd.DataFrame):
+    def _fit(self, df: pd.DataFrame) -> None:
         """Abstract method to fit the processor to the data.
 
         This method should calculate and store any parameters needed for the transformation.
@@ -29,8 +49,29 @@ class BaseProcessor(ABC):
         """
         pass
 
-    @abstractmethod
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Transforms the data based on the fitted processor.
+
+        Args:
+            df (pd.DataFrame): The input DataFrame to transform.
+
+        Returns:
+            pd.DataFrame: The transformed DataFrame.
+
+        Raises:
+            NotFittedError: If the processor is not fit before calling this method.
+            ValueError: If the specified column is not found in the DataFrame.
+        """
+        if not self.is_fit:
+            raise NotFittedError(
+                f"The processor '{self.__class__.__name__}' is not fit yet. Call `fit` first.",
+            )
+        if self.column_name not in df.columns:
+            raise ValueError(f"Column '{self.column_name}' not found in the DataFrame.")
+        return self._transform(df)
+
+    @abstractmethod
+    def _transform(self, df: pd.DataFrame) -> pd.DataFrame:
         """Abstract method to transform the data based on the fitted processor.
 
         This method should apply the transformation to the data.
