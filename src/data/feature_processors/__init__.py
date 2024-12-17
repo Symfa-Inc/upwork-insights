bdf9443e2fc60aad6414ed554667f307b8b4bc95
Michael Vinogradov <michaelvin1322@gmail.com>
2024-12-17 14:28:49 +0100

    [Feature]: updated __init__

--- src/data/feature_processors/__init__.py
+++ src/data/feature_processors/__init__.py
@@ -2,6 +2,7 @@ from boolean_processor import BooleanProcessor  # noqa: F401
 from feature_processing_pipeline import FeatureProcessingPipeline  # noqa: F401
 from freequency_processor import FrequencyProcessor  # noqa: F401
 from list_processor import ListProcessor  # noqa: F401
+from numeric_processor import NumericProcessor  # noqa: F401
 from one_hot_processor import OneHotProcessor  # noqa: F401
 from ordinal_processor import OrdinalProcessor  # noqa: F401
 from text_processor import TextProcessor  # noqa: F401
