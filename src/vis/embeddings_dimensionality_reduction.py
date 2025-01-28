10e516cf361567c48651c72a937f8f48bbb21666
Michael Vinogradov <michaelvin1322@gmail.com>
2025-01-28 13:35:29 +0100

    [Feature] removed duplicating code, created one flexible config

--- src/vis/embeddings_dimensionality_reduction.py
+++ src/vis/embeddings_dimensionality_reduction.py
@@ -11,7 +11,12 @@ from sklearn.decomposition import PCA, KernelPCA
 
 from src import PROJECT_DIR
 from src.data.feature_processors.ppa import PCAWithPreProcessing
-from src.data.utils import get_embeddings, load_model_from_pickle, save_model_to_pickle
+from src.data.utils import (
+    get_embeddings,
+    get_embeddings_gte,
+    load_model_from_pickle,
+    save_model_to_pickle,
+)
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -212,7 +217,7 @@ def visualize_variances(
 
 @hydra.main(
     config_path=os.path.join(PROJECT_DIR, 'configs'),
-    config_name='embeddings_dimensionality_reduction_openai',
+    config_name='embeddings_dimensionality_reduction',
     version_base=None,
 )
 def main(cfg: DictConfig) -> None:
@@ -221,36 +226,50 @@ def main(cfg: DictConfig) -> None:
     # Define absolute paths
     data_path = str(os.path.join(PROJECT_DIR, cfg.data_path))
     embeddings_path = str(os.path.join(PROJECT_DIR, cfg.embeddings_path))
-    save_dir = str(os.path.join(PROJECT_DIR, cfg.save_dir))
+
+    embeddings_model_url = cfg.embeddings_model_url
+    embeddings_model = embeddings_model_url.split('/')[-1]
+    embeddings_function = (
+        get_embeddings if embeddings_model.startswith('text-embedding-3') else get_embeddings_gte
+    )
+
+    save_dir = str(os.path.join(PROJECT_DIR, cfg.save_dir + embeddings_model))
+
+    max_n_components = cfg.max_n_components[embeddings_model_url]
+    batch_size = cfg.batch_size
+
     os.makedirs(save_dir, exist_ok=True)
 
     text_columns = cfg.text_columns
     missing_columns = [
         column
         for column in text_columns
-        if not os.path.exists(
-            os.path.join(embeddings_path, f"{column}_text-embedding-3-large.parquet"),
-        )
+        if not os.path.exists(os.path.join(embeddings_path, f"{column}_{embeddings_model}.parquet"))
     ]
 
     if missing_columns:
-        calculate_and_save_embeddings(data_path, embeddings_path, missing_columns)
+        calculate_and_save_embeddings(
+            data_path,
+            embeddings_path,
+            missing_columns,
+            batch_size=batch_size,
+            model=embeddings_model_url,
+            model_function=embeddings_function,
+        )
 
     methods: Dict[str, Tuple[Type[Union[PCA, KernelPCA, PCAWithPreProcessing]], dict]] = {
-        'PCA': (PCA, dict(n_components=3072)),
+        'PCA': (PCA, dict(n_components=max_n_components)),
         'PCAWithPreProcessing (normalize)': (
             PCAWithPreProcessing,
-            dict(n_components=3072, preprocessing_method='normalize'),
+            dict(n_components=max_n_components, preprocessing_method='normalize'),
         ),
         'PCAWithPreProcessing (standardize)': (
             PCAWithPreProcessing,
-            dict(n_components=3072, preprocessing_method='standardize'),
+            dict(n_components=max_n_components, preprocessing_method='standardize'),
         ),
     }
     for column in text_columns:
-        df = pd.read_parquet(
-            os.path.join(embeddings_path, f"{column}_text-embedding-3-large.parquet"),
-        )
+        df = pd.read_parquet(os.path.join(embeddings_path, f"{column}_{embeddings_model}.parquet"))
         explained_variances, cumulative_variances = fit_dimensionality_reduction_methods(
             df,
             column,
