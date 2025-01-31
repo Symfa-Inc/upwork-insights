1bb3dd00e2df8fef105726d19475243a2b562135
Michael Vinogradov <michaelvin1322@gmail.com>
2025-01-31 14:01:16 +0200

    [Feature]: modified text-processor to be abstract. It only calculates embeddings, but logic will be defined by child classes

--- src/data/feature_processors/text_processor.py
+++ src/data/feature_processors/text_processor.py
@@ -1,46 +1,53 @@
+import os
+from abc import ABC, abstractmethod
 from typing import List
 
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import PCA
 
 from src.data.feature_processors.base_processor import BaseProcessor
-from src.data.utils import get_embeddings
+from src.data.utils import get_embeddings, get_embeddings_gte
 
 
-class TextProcessor(BaseProcessor):
-    """A processor for handling and transforming text data into PCA of embeddings.
-
-    Attributes:
-        column_name (str): The name of the column to process.
-        pca_threshold (float): Explained variance ratio threshold for PCA.
-        pca (Optional[PCA]): PCA instance, fitted during the `fit` method.
-    """
+class TextProcessor(BaseProcessor, ABC):
+    """Abstract base class for text processing with different transformation techniques."""
 
     column_name: str
-    pca_threshold: float
 
     def __init__(
         self,
         column_name: str,
-        pca_threshold: float = 0.85,
+        model: str = 'thenlper/gte-small',
+        embeddings_dir: str = './embeddings',
     ):
-        """Initializes the TextProcessor with OpenAI client, PCA threshold, and optional scaler.
+        """Initializes the TextProcessor with a model for generating embeddings.
 
         Args:
             column_name (str): The name of the column to process.
-            pca_threshold (float): Explained variance ratio threshold for PCA.
-
-        Raises:
-            ValueError: If `pca_threshold` is not between 0 and 1.
+            model (str): The embedding model to use. Defaults to 'thenlper/gte-small'.
+            embeddings_dir (str): Directory where embeddings should be stored. Defaults to './embeddings'.
         """
-        if not (0 < pca_threshold <= 1):
-            raise ValueError('pca_threshold must be between 0 and 1.')
+        valid_models = {
+            'text-embedding-3-small',
+            'text-embedding-3-large',
+            'thenlper/gte-small',
+            'thenlper/gte-large',
+        }
+        if model not in valid_models:
+            raise ValueError(f"Invalid model: {model}. Must be one of {valid_models}")
 
-        self.pca_threshold = pca_threshold
-
-        # Call parent class initializer
+        self.model = model
+        self.model_name = model.split('/')[-1]
+        self.embeddings_dir = embeddings_dir
         super().__init__(column_name)
+
+    @abstractmethod
+    def _fit(self, df: pd.DataFrame):
+        pass
+
+    @abstractmethod
+    def _transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        pass
 
     def _get_embeddings(
         self,
@@ -48,116 +55,55 @@ class TextProcessor(BaseProcessor):
         batch_size: int = 1000,
         model: str = 'text-embedding-3-large',
     ) -> np.ndarray:
-        """Generates embeddings for a list of strings using OpenAI's API.
+        """Generates embeddings for a list of strings using the specified model.
 
         Args:
             texts (List[str]): A list of strings to generate embeddings for.
-            batch_size (int): The size of the batches for API requests. Defaults to 100.
+            batch_size (int): The size of the batches for API requests. Defaults to 1000.
+            model (str): The embedding model to use. Defaults to 'text-embedding-3-large'.
 
         Returns:
-            np.ndarray: A numpy array of embeddings.
+            np.ndarray: A numpy array containing the generated embeddings.
         """
-        return get_embeddings(texts, batch_size, model)
+        if self.model in {'text-embedding-3-small', 'text-embedding-3-large'}:
+            return get_embeddings(texts, batch_size, model=model)
+        elif self.model in {'thenlper/gte-small', 'thenlper/gte-large'}:
+            return get_embeddings_gte(texts, batch_size, model=model)
+        else:
+            raise ValueError(f"Unsupported model: {self.model}")
 
-    def _fit(self, df: pd.DataFrame):
-        """Fits the processor by generating embeddings, scaling them, applying PCA.
+    def _load_or_compute_embeddings(self, texts: pd.Series) -> pd.DataFrame:
+        """Loads embeddings from a file if available, otherwise computes and saves missing ones.
 
         Args:
-            df (pd.DataFrame): The input DataFrame to fit on.
-
-        Raises:
-            ValueError: If the column is not found in the DataFrame.
-        """
-        texts = df[self.column_name].tolist()
-        embeddings = self._get_embeddings(texts)
-
-        # Initial PCA fit to compute explained variance
-        self.pca = PCA()
-        self.pca.fit(embeddings)
-        cumulative_variance = np.cumsum(self.pca.explained_variance_ratio_)
-
-        # Select the optimal number of components
-        n_components = np.argmax(cumulative_variance >= self.pca_threshold) + 1
-
-        # Refit PCA with the optimal number of components
-        if n_components < embeddings.shape[1]:
-            self.pca = PCA(n_components=n_components)
-            self.pca.fit(embeddings)
-
-    def _transform(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Transforms the data by generating embeddings, scaling them, and applying PCA.
-
-        Args:
-            df (pd.DataFrame): The input DataFrame to transform.
+            texts (pd.Series): A series containing text data.
 
         Returns:
-            pd.DataFrame: The transformed DataFrame with reduced-dimensionality embeddings.
-
-        Raises:
-            ValueError: If the processor has not been fitted or the column is missing.
+            pd.DataFrame: A DataFrame with the same index as `texts`, and embedding features as columns.
         """
-        # Generate embeddings
-        texts = df[self.column_name].tolist()
-        embeddings = self._get_embeddings(texts)
+        file_path = os.path.join(
+            self.embeddings_dir,
+            f"{self.column_name}_{self.model_name}.parquet",
+        )
+        if os.path.exists(file_path):
+            existing_embeddings = pd.read_parquet(file_path)
+        else:
+            existing_embeddings = pd.DataFrame()
 
-        # Apply PCA
-        embeddings = self.pca.transform(embeddings)
+        missing_indexes = texts.index.difference(existing_embeddings.index)
+        if not missing_indexes.empty:
+            new_texts = texts.loc[missing_indexes].tolist()
+            new_embeddings = self._get_embeddings(new_texts)
+            new_embedding_df = pd.DataFrame(
+                new_embeddings,
+                index=missing_indexes,
+                columns=[f"Embedding_{i+1}" for i in range(new_embeddings.shape[1])],
+            )
+            existing_embeddings = pd.concat([existing_embeddings, new_embedding_df])
+            existing_embeddings.to_parquet(file_path)
 
-        # Create a DataFrame for principal components
-        pc_columns = {
-            f"{self.column_name}_PC{i + 1}": embeddings[:, i] for i in range(embeddings.shape[1])
-        }
-        pc_df = pd.DataFrame(pc_columns, index=df.index)
+        return existing_embeddings.loc[texts.index]
 
-        # Drop the original column and concatenate new principal components
-        df = df.drop(columns=[self.column_name])
-        df = pd.concat([df, pc_df], axis=1)
-        return df
-
+    @abstractmethod
     def get_params(self) -> dict:
-        return {
-            'PCA': {
-                'n_params': int(self.pca.n_components_),
-                'explained_variance': self.pca.explained_variance_ratio_.tolist(),
-                'components': self.pca.components_,
-            },
-        }
-
-
-if __name__ == '__main__':
-    data = pd.DataFrame(
-        {
-            '_text_column': [
-                'This is the first test sentence.',
-                'Here is another example for testing.',
-                'Machine learning with embeddings is powerful.',
-                '',
-                'OpenAI API provides useful tools.',
-                'Testing the PCA transformation process.',
-            ],
-            'text_column': [
-                'This is the first test sentence.',
-                'Here is another example for testing.',
-                'Machine learning with embeddings is powerful.',
-                '',
-                'OpenAI API provides useful tools.',
-                'Testing the PCA transformation process.',
-            ],
-        },
-    )
-
-    processor = TextProcessor(
-        column_name='text_column',
-        pca_threshold=0.85,
-    )
-
-    # Transform the data
-    transformed_data = processor.process(data)
-
-    # Output transformed data
-    print('Transformed Data:')
-    print(transformed_data)
-
-    # Output processor parameters
-    print('\nProcessor Parameters:')
-    print(processor.get_params())
+        pass
